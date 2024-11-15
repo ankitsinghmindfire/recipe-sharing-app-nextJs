84110cee70f8474da1ba8ef7a69e2ffc0a7ee357
ankitsinghmindfire <ankit.singh@mindfiresolutions.com>
2024-11-15 17:04:03 +0530

    testing git husky

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+    let a =10;
     return (
         <div className={styles.page}>
             <main className={styles.main}>
